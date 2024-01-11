6762d91e6dcdb31647081b7f05819f937b323e22
Juhani Astikainen <juhani.astikainen@gmail.com>
2024-01-11 14:59:31 +0200

    tehtävä14

--- src/App.js
+++ src/App.js
@@ -12,11 +12,39 @@ function App() {
     return (
         <>
             <div>
-                <RecipeCard
-                    title="My awesome kebab"
-                    desc="Very tasty kebab with lamb meat and hot spices, which we can cook on the grill in the winter when outside is freezing cracking show and inside the fire cracks in the stove"
-                    picurl="https://images.pexels.com/photos/8963961/pexels-photo-8963961.jpeg"
-                />
+                <Row style={{paddingTop: "20px"}}>
+                    <Col md="auto" style={{paddingLeft: "30px"}}>
+                    <RecipeCard
+                        title="My awesome kebab"
+                        desc="Very tasty kebab with lamb meat and hot spices, which we can cook on the grill in the winter when outside is freezing cracking show and inside the fire cracks in the stove"
+                        picurl="https://images.pexels.com/photos/8963961/pexels-photo-8963961.jpeg"
+                    />
+                    </Col>                    
+                    <Col md="auto" style={{paddingLeft: "10px"}}>
+                    <RecipeCard
+                        title="My awesome kebab"
+                        desc="Very tasty kebab with lamb meat and hot spices, which we can cook on the grill in the winter when outside is freezing cracking show and inside the fire cracks in the stove"
+                        picurl="https://images.pexels.com/photos/8963961/pexels-photo-8963961.jpeg"
+                    />
+                    </Col>
+                </Row>
+                <Row style={{paddingTop: "20px"}}>
+                    <Col md="auto" style={{paddingLeft: "30px"}}>
+                    <RecipeCard
+                        title="My awesome kebab"
+                        desc="Very tasty kebab with lamb meat and hot spices, which we can cook on the grill in the winter when outside is freezing cracking show and inside the fire cracks in the stove"
+                        picurl="https://images.pexels.com/photos/8963961/pexels-photo-8963961.jpeg"
+                    />
+                    </Col>                    
+                    <Col md="auto" style={{paddingLeft: "10px"}}>
+                    <RecipeCard
+                        title="My awesome kebab"
+                        desc="Very tasty kebab with lamb meat and hot spices, which we can cook on the grill in the winter when outside is freezing cracking show and inside the fire cracks in the stove"
+                        picurl="https://images.pexels.com/photos/8963961/pexels-photo-8963961.jpeg"
+                    />
+                    </Col>
+                </Row>
+
             </div>
         </>
     );
