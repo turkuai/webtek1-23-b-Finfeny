de06625c2c3327fb7795068a92a2db530fd6c6b7
Juhani Astikainen <juhani.astikainen@gmail.com>
2024-01-18 11:03:09 +0200

    tehtävä2.3

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,16 +1,42 @@
-import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import React, { useState , useEffect } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import Form from 'react-bootstrap/Form';
 
 function RecipeItem({ name, amount }) {
+
+    const [edit, setEdit] = useState(false);
+    const toggle = () => setEdit(!edit);
+    const [nameValue, setNameValue] = useState(name);
+    const [amountValue, setamountValue] = useState(amount);
+
     return (
         <Container>
             <Row>
-                <Col>{name}</Col>
-                <Col>{amount}</Col>
+                {
+                edit ?  <Col>
+                            <Form.Control hidden={!edit} type="text" value={nameValue} onChange={(eventName) => {setNameValue(eventName.target.value)}}/>
+                        </Col>
+                        : <></>
+                }
+                {
+                edit ?  <Col>
+                            <Form.Control hidden={!edit} type="number" value={amountValue} onChange={(eventAmount) => {setamountValue(eventAmount.target.value)}}/>
+                        </Col>
+                        : <></>
+                }
+                <Col hidden={edit}>{nameValue}</Col>
+                <Col style={{textAlign: "center"}} hidden={edit}>{amountValue}</Col>
+                <Col>
+                    <Button variant="primary" onClick={toggle} hidden={edit}>
+                    <i class="bi bi-pencil-square"></i>
+                    </Button>
+                    <Button variant="success" onClick={toggle}  hidden={!edit}>
+                    <i class="bi bi-check-square"></i>
+                    </Button>
+                </Col>
             </Row>
         </Container>
     );
-
 }
 
 export default RecipeItem;
