4de934aee7c665ca643ece58e890996ef49a15bc
Juhani Astikainen <juhani.astikainen@gmail.com>
2024-01-19 14:12:57 +0200

    2.6

--- src/components/RecipeDialog.js
+++ src/components/RecipeDialog.js
@@ -3,24 +3,30 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'react-bootst
 import Form from 'react-bootstrap/Form';
 import ListGroup from "./ListGroup"
 
-
 function RecipeDialog({title, desc, picurl, show, setShow}) {
   
   const [editTitle, setEditTitle] = useState(false);
   const toggleTitle = () => setEditTitle(!editTitle);
   const [titleValue, setTitleValue] = useState(title);
   
-  const [editDesc, setEditDesc] = useState(false);
-  const toggleDesc = () => setEditDesc(!editDesc);
   const [descValue, setDescValue] = useState(desc);
+  const [editDesc, setEditDesc] = useState(false);
+  const toggleDesc = () => {
+    setEditDesc(!editDesc);
+    ref.current=descValue;
+    console.log(descValue)
+  }
 
-  const ref = useRef(desc);
+  const ref = useRef(descValue);
 
   return (
     <div>
       <Modal show={show}>
         <Modal.Header /*closeButton={toggle}*/>
-          <Form.Control hidden={!editTitle} type="text" value={titleValue} onChange={(eventTitle) => {console.log(eventTitle.target.value); setTitleValue(eventTitle.target.value)}}/>
+          <Form.Control hidden={!editTitle} type="text" value={titleValue} onChange={(eventTitle) => {console.log(eventTitle.target.value);
+            setTitleValue(eventTitle.target.value)
+            }}/>
+
           <Modal.Title hidden={editTitle}>{titleValue}</Modal.Title>
           <Button variant={editTitle ? "success" : "primary"} onClick={toggleTitle}>
           <i class={editTitle ? "bi bi-check-square" : "bi bi-pencil-square"}></i>
@@ -29,11 +35,13 @@ function RecipeDialog({title, desc, picurl, show, setShow}) {
         <Modal.Body>
             <img src={picurl} className='img-fluid'/>
             <p hidden={editDesc}>{descValue}</p>
-            <Form.Control hidden={!editDesc}  as="textarea" rows={3} style={{marginBlock: "10px"}} value={descValue} onChange={(eventDesc) => {console.log(eventDesc.target.value); setDescValue(eventDesc.target.value)}}/>
+            <Form.Control hidden={!editDesc}  as="textarea" rows={3} style={{marginBlock: "10px"}} value={descValue} onChange={(eventDesc) => {
+              setDescValue(eventDesc.target.value);}}/>
+            
             <Button variant={editDesc ? "success" : "primary"} onClick={toggleDesc} className="mb-3">
             <i class={editDesc ? "bi bi-check-square" : "bi bi-pencil-square"}></i>
             </Button>
-            <Button hidden={!editDesc} variant="light" onClick={() => {console.log(ref.current); setDescValue(ref.current)}} className="mb-3" style={{ marginLeft: "10px" }}>
+            <Button hidden={!editDesc} variant="light" onClick={() => {descValue==ref.current ? setDescValue(desc) : setDescValue(ref.current)}} className="mb-3" style={{ marginLeft: "10px" }}>
             <i class="bi bi-arrow-counterclockwise"></i>
             </Button>
             <ListGroup></ListGroup>
